bccc33d3a092c50a91ec62e9825a97f1641582e1
yukigam <keed80282@gmail.com>
2026-03-11 13:15:42 +0800

    fix layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,6 @@
-export default function RootLayout({ children }) {
+import { ReactNode } from "react";
+
+export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="mn">
       <body>{children}</body>

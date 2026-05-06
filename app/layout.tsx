7c9f2cd07d171bfd898b804f524b8b6a6a2a5a6a
yukigam <keed80282@gmail.com>
2026-05-06 20:23:40 +0800

    feat: add google analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,13 @@
 import { ReactNode } from "react";
+import GoogleAnalytics from "../components/GoogleAnalytics";
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="mn">
-      <body>{children}</body>
+      <body>
+        <GoogleAnalytics />
+        {children}
+      </body>
     </html>
   );
 }

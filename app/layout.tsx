2dafc3b7801dd7aecf7d7fe2258975e9f1a66092
yukigam <keed80282@gmail.com>
2026-05-06 21:02:28 +0800

    chore: add google site verification

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,12 @@
 import { ReactNode } from "react";
+import type { Metadata } from "next";
 import GoogleAnalytics from "../components/GoogleAnalytics";
+
+export const metadata: Metadata = {
+  verification: {
+    google: "ANxdo5-GCya_--dWhCSLTk9pSh3emfgEw_Y75MCgooU",
+  },
+};
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (

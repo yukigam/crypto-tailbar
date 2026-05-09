4c4c8eb9cccfc430057ecfca1577241b42500519
yukigam <keed80282@gmail.com>
2026-05-09 18:35:45 +0800

    fix: disable global caching in layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,6 +9,8 @@ export const metadata: Metadata = {
   },
 };
 
+export const revalidate = 0;
+
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="mn">

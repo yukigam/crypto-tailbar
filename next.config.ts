771c8062760aed3677cb1c6e2fe6d74e63fe03e5
yukigam <keed80282@gmail.com>
2026-05-09 19:16:11 +0800

    fix: aggressively disable Vercel and Sanity caching

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,17 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  headers: async () => [
+    {
+      source: '/(.*)',
+      headers: [
+        {
+          key: 'Cache-Control',
+          value: 'no-store, max-age=0, must-revalidate',
+        },
+      ],
+    },
+  ],
 };
 
 export default nextConfig;

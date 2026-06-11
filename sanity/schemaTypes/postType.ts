a27ffacb18358130aedd3c07a2128b1b69cd045d
yukigam <keed80282@gmail.com>
2026-06-11 14:15:10 +0800

    засвар: category талбарыг optional болгосон (required устгасан)

--- sanity/schemaTypes/postType.ts
+++ sanity/schemaTypes/postType.ts
@@ -57,7 +57,6 @@ export const postType = defineType({
         ],
         layout: 'dropdown',
       },
-      validation: (Rule) => Rule.required(),
     }),
     defineField({
       name: 'categories',
